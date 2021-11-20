4dfc051034d03222f587f3930524d3aee5c8ed05
David Davies <David_Davies@mail.com>
2021-11-20 11:48:54 +0000

    added queryselectors

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,4 +1,7 @@
 $(document).ready(function () {
+  var searchLibFor = document.querySelector("#srchFor");
+  var itemFormatSelect = document.querySelector("#dropdownMenuButton");
+  var submitSelection = document.querySelector("#srchSubmit");
 $("#backMain").on("click", function () {
   location.replace("../../index.html");
 });
